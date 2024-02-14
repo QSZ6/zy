fbf968b22f2938cec58d6ad7dcaa7e9bd5af67a4
QSZ6 <972560507@qq.com>
2024-02-14 16:36:07 +0800

    3
    3

---
+++ ss/js/photo.js
@@ -0,0 +1,123 @@
+const photosInfo = [
+    {
+        cephalogram_url: '../image/life1/1.jpg',
+        name: '龙猫',
+        introduction: '善良的孩子会遇见龙猫哦！',
+        size: 'width-24',
+        children: [
+            {
+                children_url: '../image/life1/2.jpg',
+                children_introduction: `什么时候我们开始无法像孩子一样肆意地大呼小叫了？心里的小情绪堆积的像山一样高，
+                直到溢出来。与此同时，不如永远像孩子一样。`,
+            },
+            {
+                children_url: '../image/life1/3.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: '../image/life1/4.jpg',
+                children_introduction: `认清这个世界，然后爱他。就这么一辈子，下一世你可能就不在这个世界了。
+                有些烦恼，丢掉了才有云淡风轻的机会。
+                心存善意，定能途遇天使。`,
+            },
+            {
+                children_url: '../image/life1/5.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: '../image/life1/6.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: '../image/life1/7.jpg',
+                children_introduction: `也许在这个美丽的童话故事里，没有什么明显的主题。它仿佛是在叙述一个普普通通的故事：
+                平淡的开始，平淡的结局 ，总是让人觉得亲切，也总是有些期盼`,
+            },]
+    },
+    {
+        cephalogram_url: '../image/life2/1.jpg',
+        name: '名侦探柯南',
+        introduction: '每个人心中都有个迷，但却隐隐闪烁着光芒',
+        size: 'width-11',
+        children: [
+            {
+                children_url: '../image/life2/2.jpg',
+                children_introduction: `话只要一说出口，就没有办法再收回去了。人讲的话就像刀刃，一旦用错了地方，
+                就会变成棘手的凶器。甚至还有人因为口角误会，失去了一生的好朋友。一旦错过的话，就很可能再也见不到面了。`,
+            },
+            {
+                children_url: '../image/life2/3.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: '../image/life2/4.jpg',
+                children_introduction: `我并不讨厌等待，因为等待的越久，见到他的时候，就会越开心。`,
+            },
+            {
+                children_url: '../image/life2/5.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: '../image/life2/6.jpg',
+                children_introduction: '',
+            },]
+    },
+    {
+        cephalogram_url: '../image/life3/1.jpg',
+        name: '夏目友人帐',
+        introduction: '生命中总有不期而遇的惊喜和生生不息的希望',
+        size: 'width-11',
+        children: [
+            {
+                children_url: '../image/life3/2.jpg',
+                children_introduction: `虽然岁月被拉的太过悠长，让我几乎忘记最初的阳光，我也要感谢你，
+                曾经用那么温柔的行动给了我一个，那么五彩斑斓的梦。`,
+            },
+            {
+                children_url: '../image/life3/3.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: '../image/life3/4.jpg',
+                children_introduction: `我必须承认，生命中大部分时光是属于孤独的，努力成长是在孤独中可以做到的最好的事情。`,
+            },
+            {
+                children_url: '../image/life3/5.jpg',
+                children_introduction: '',
+            },]
+    },
+    {
+        cephalogram_url: '../image/life4/1.jpg',
+        name: '萤火之森',
+        introduction: '过来，萤，我终于能触碰你了',
+        size: 'width-24',
+        children: [
+            {
+                children_url: '../image/life4/2.jpg',
+                children_introduction: `每个女孩心里都有一片萤火之森，那里住着一个少年。`,
+            },
+            {
+                children_url: '../image/life4/3.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: '../image/life4/4.jpg',
+                children_introduction: `如果时光可以倒流，我还是会选择认识你，虽然会伤痕累累，但是心中的温暖记忆是谁都无法给予的，
+                谢谢你来过我的世界。`,
+            },
+            {
+                children_url: '../image/life4/5.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: '../image/life4/6.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: '../image/life4/7.jpg',
+                children_introduction: `失去只是一瞬，一瞬的恍惚。这段时间，无法在盼望夏天了。多幸运能在消失前抱一下你，
+                只是我始终没能说出那句：我喜欢你。`,
+            },]
+    }
+]
+
